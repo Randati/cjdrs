fcf90821ed9cfda8df7edb0db86f251f536e6598
Vanhala Antti <antti.vanhala@aalto.fi>
2015-01-03 14:52:52 +0200

    Divide different commands into different functions in main()

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,6 @@ Options:
 Configuration file defaults to 'cjdrs.conf' if not given.
 ");
 
-
-
 #[cfg(not(test))]
 fn main() {
 	let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
@@ -38,50 +36,51 @@ fn main() {
 
 	cjdrs::init();
 
-
-
-	// Generate new configuraion file
 	if args.cmd_init {
-		let identity = PrivateIdentity::generate();
-		
-		let config = Config::get_default(&identity);
-		config.write(&config_path).unwrap();
-
-		println!("Created new configuration file '{}'", config_path.display());
-		println!("Public key: {}", identity.public_key.as_string());
-		println!("Address:    {}", identity.address);
-		return;
+		init_config(&config_path);
+	} else {
+		assert!(args.cmd_run);
+		let config = Config::load(&config_path);
+		run(&config);
 	}
+}
 
-	// Otherwise continue running
-	assert!(args.cmd_run);
+#[cfg(not(test))]
+fn init_config(config_path: &Path) {
+	let identity = PrivateIdentity::generate();
+		
+	let config = Config::get_default(&identity);
+	config.write(config_path).unwrap();
 
+	println!("Created a new configuration file '{}'", config_path.display());
+	println!("Public key: {}", identity.public_key.as_string());
+	println!("Address:    {}", identity.address);
+}
 
-	let config = Config::load(&config_path);
-	let identity = {
+#[cfg(not(test))]
+fn run(config: &Config) {
+	let my_identity = {
 		let private_key = PrivateKey::from_string(config.privateKey.as_slice()).unwrap();
 		PrivateIdentity::from_private_key(&private_key).unwrap()
 	};
 
-	println!("Public key:  {}", identity.public_key.as_string());
-	println!("Address:     {}", identity.address);
+	println!("Public key: {}", my_identity.public_key.as_string());
+	println!("Address:    {}", my_identity.address);
 	
 
 	let tun_interface = interface::Tun::new(
 		config.tunDevice.as_slice(),
-		&identity.address);
+		&my_identity.address);
 	println!("Opened tun device '{}'", tun_interface.get_name());
 
-	
 	let udp_interface = interface::Udp::create(config.udpBind.as_slice());
 
-
-	let router = Router::new(&identity.address);
+	let router = Router::new(&my_identity.address);
 
 
 	let mut mio_loop: mio::EventLoop<uint, ()> = mio::EventLoop::new().unwrap();
 	let event_handler = EventHandler::new(&mut mio_loop,
-		identity,
+		my_identity,
 		tun_interface,
 		udp_interface,
 		router).ok().expect("Couldn't create the event handler");
